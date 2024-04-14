c2872b118fc24f1499c2fc7d110a2ff79d0c1917
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-04-14 15:47:50 +0200

    Bouton d'accès aux règles du jeu

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     <title>Pierre Feuille Ciseaux</title>
   </head>
   <body>
@@ -13,17 +13,21 @@
         <h1 class="bandeau shadow-lg p-3 mt-2 mb-5 rounded text-center">
             Bonjour et bienvenue sur notre jeu culte ! </h1>
     </div>
-    <div class="container text-center mb-5 ">
-        <button type="button" class="bandeau p-2 mt-2 mb-2  rounded text-center" id="rejouer" >Rejouer</button>
+    <div class="container text-center mb-3 ">
+      <a href="#regles" class="nav nav-link bandeau p-2 mt-2 mb-2 rounded" id="reglesDuJeu">
+          Accéder aux règle du jeu
+      </a>
+        <button type="button" class="bandeau p-2 mt-2 mb-2  rounded" id="rejouer" >Rejouer</button>
+    </div>
     </div>
     <div class="container text-center ">
         <img src="/assets//img/Pierre.png" alt="pierre" id="pierre" class="green" >
         <img src="/assets/img/Feuille.png " alt="Feuille" id="feuille"  class="green" >
         <img src="/assets/img/Ciseaux.png " alt="Ciseaux" id="ciseaux"  class="green" >
+        <h3  class="mt-5" id="regles">Règles du jeu</h3>
     </div>
 
     <div class="container text-center mt-5 bandeau p-3 rounded" id="message">
-        Messages
     </div>
     <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 // Création des constantes de récupération des items par leur Id
 const imagePierre = document.getElementById("pierre");
-
 const imageFeuille = document.getElementById("feuille");
-
 const imageCiseaux = document.getElementById("ciseaux");
-
 const message =document.getElementById("message");
-
 const rejouer = document.getElementById("rejouer");
+const regles = document.getElementById("regles");
+const reglesDuJeu = document.getElementById("reglesDuJeu");
 
+reglesDuJeu.addEventListener("click", afficherRegles);
 
+function afficherRegles(){
+    regles.setAttribute("style","visibility:visible;  height: 100%;");
+}
+
+// Ajout d'évènement sur e bouton "Rjouer"
 rejouer.addEventListener("click", function(){init();})
-
 
 // Définition du choix de l'ordi et les choix possibles
 let choixOrdinateur;
 const choixPoissible = ["PIERRE", "FEUILLE", "CISEAUX"];
 
-function init(){
-        
+function init(){     
     // Ajout d'évènement à chaque image
     imagePierre.addEventListener("click", clickPierre);
     imageFeuille.addEventListener("click", clickFeuille);
@@ -32,6 +34,7 @@ function init(){
     message.innerHTML = "Le choix de l'ordinateur est fait, à vous de jouer...";
     
 }
+
 // Création des fonctions
 function clickPierre(){
     imagePierre.style.border = "thick solid #0000FF";
@@ -47,21 +50,19 @@ function clickCiseaux(choixJoueur){
     imageCiseaux.style.border = "thick solid #0000FF";
     verification(2);
 }
-
+//  Création de la fonction pour comparer les choix entre jouer et ordinateur
 function verification(choixJoueur){
     let gagnant;
     if(choixJoueur == choixOrdinateur){
-        gagnant = "Match nul !";
+        gagnant = "Match nul, retantez votre choix !";
     } else if((choixJoueur == 0 && choixOrdinateur == 2) || (choixJoueur == 1 && choixOrdinateur == 2) || (choixJoueur == 2 && choixOrdinateur == 1)){
-        gagnant = "Vous avez gagné";
+        gagnant = "Bravo vous avez gagné";
     } else {
-        gagnant = "L'ordinateur à gagné";
+        gagnant = "Dommage, l'ordinateur à gagné";
     }
     message.innerHTML = "Le choix de l'ordinateur est : " + choixPoissible[choixOrdinateur] + "<br>" + "Votre choix est " + choixJoueur + "<br>" + gagnant;
 
     // Suppression des évènements
-        
-
     imagePierre.removeEventListener("click", clickPierre);
     imageFeuille.removeEventListener("click", clickFeuille);
     imageCiseaux.removeEventListener("click", clickCiseaux);
